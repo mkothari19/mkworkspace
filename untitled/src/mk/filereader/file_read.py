9dd96fefdb58168362925faadac7b8f0ea087b28
mkothari19 <mkothari19@users.noreply.github.com>
2025-01-21 11:58:19 +0530

    delete project

--- untitled/src/mk/filereader/file_read.py
+++
@@ -1,9 +0,0 @@
-def read_file(file_path):
-    """Read file and return  its content"""
-    try:
-        with open(file_path, 'r') as file:
-            return file.readlines()
-    except FileNotFoundError:
-        raise FileNotFoundError(f"file not found error:{file_path}")
-    except Exception as e:
-        raise Exception(f"an error occurred:{e}")
--- untitled/src/utils.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello, POC!")
-
-
-if __name__ == "__main__":
-    main()
